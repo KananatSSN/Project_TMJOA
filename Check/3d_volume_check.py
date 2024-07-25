c22537ed43ce55abddd54df189fcfef0edc672db
Kananat <kananat.ssn@gmail.com>
2024-07-25 16:29:14 +0700

    Script to visually check all 3d volume in a folder

---
+++ Check/3d_volume_check.py
@@ -0,0 +1,65 @@
+# exec(open(r"C:\Users\acer\Desktop\Project_TMJOA\Check\3d_volume_check.py").read())
+# This script is use to check the 3d volume in 'input_folder' and save the problemetic files's name in 'output_file'
+import os, sys
+import slicer
+from slicer.ScriptedLoadableModule import *
+import vtk, qt, ctk
+
+# Define the folder containing the .nii files
+input_folder = r"C:\Users\acer\Desktop\Data_Prep_3\CropTMJ2"
+output_file = r"C:\Users\acer\Desktop\Data_Prep_3\Problem_file.txt"
+
+with open(output_file, 'w') as log_file:
+    log_file.write('')  # Optionally write a header or leave it completely empty
+
+files = os.listdir(input_folder)
+number_of_files = len(files)
+
+print(f"There are {number_of_files} .nii.gz files in the {input_folder}")
+
+progress = 0
+
+# Load the text file for writing if needed
+with open(output_file, 'w') as log_file:
+    # Loop over each file in the directory
+    for filename in os.listdir(input_folder):
+        progress += 1
+        print(f"[Processing {progress} out of {number_of_files}]")
+
+        if filename.endswith('.nii.gz'):
+            file_path = os.path.join(input_folder, filename)
+            # Load the volume
+            loadedVolume = slicer.util.loadVolume(file_path)
+            
+            # Create a volume rendering of the loaded image
+            volumeRenderingLogic = slicer.modules.volumerendering.logic()
+            displayNode = volumeRenderingLogic.CreateDefaultVolumeRenderingNodes(loadedVolume)
+            displayNode.GetVolumePropertyNode().Copy(volumeRenderingLogic.GetPresetByName('CT-AAA'))
+            slicer.app.layoutManager().resetThreeDViews()
+            displayNode.SetVisibility(True)
+            slicer.app.processEvents()
+
+            # Wait for user input to continue or log the file
+            while True:
+                user_input = input(f"{filename}\n[ok/no] : ").lower()
+                slicer.app.processEvents()
+                if user_input == 'ok':
+                    slicer.app.processEvents()
+                    break
+                elif user_input == 'no':
+                    log_file.write(filename + '\n')
+                    #log_file.flush()
+                    print(f"Logged {filename}")
+                    slicer.app.processEvents()
+                    break
+                elif user_input == 'stop':
+                    print(f"Stopping the program")
+                    slicer.app.processEvents()
+                    sys.exit()
+                else:
+                    print("Invalid input, please type 'ok' or 'no'.")
+                slicer.app.processEvents()
+
+            slicer.mrmlScene.RemoveNode(loadedVolume)
+
+print("Processing complete.")
