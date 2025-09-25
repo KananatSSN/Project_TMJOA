254177ea89747b6a15eb7efa27069a15862cec56
KananatSSN <kananat.ssn@gmail.com>
2025-09-25 14:05:19 +0700

    Save raw prediction results

--- test_prediction_by_id.py
+++ test_prediction_by_id.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import os
 import pprint
+import csv
 
 def calculate_metrics(predictions, classes_name = ['0', '1']):
     """
@@ -69,8 +70,52 @@ def predict_image_folder(model, image_folder_path, conf_threshold = 0.7):
         return '1'
     else:
         return '0'
+    
+def get_prediction_results(model, testset_folder_path):
 
-def predict_by_ID(model, test_folder):
+    result_from_images = []
+
+    classes_name = os.listdir(testset_folder_path)
+    for class_name in classes_name:
+       
+        class_folder_path = os.path.join(testset_folder_path, class_name)
+        if not os.path.isdir(class_folder_path):
+            continue
+
+        print(f"Processing class {class_name}")
+
+        patients_image_folder = os.listdir(class_folder_path)
+        real = class_name
+        for patient_image_folder in patients_image_folder:
+            print(f"Processing patient {patient_image_folder}")
+            patient_image_folder_path = os.path.join(class_folder_path, patient_image_folder)
+            patient_id = patient_image_folder
+
+            images_name = os.listdir(patient_image_folder_path)
+            for image_name in images_name:
+
+                image_path = os.path.join(patient_image_folder_path, image_name)
+
+                if not os.path.exists(image_path):
+                    print(f"Image path does not exist: {image_path}")
+                    continue
+                    
+                results = model(image_path, verbose = False)
+                result = results[0].probs
+
+                if result :
+                    (predicted_class, conf) = (result.top1, result.top1conf.item())
+                    result_from_images.append((patient_id ,real, predicted_class, conf))
+
+    save_path = rf"{testset_folder_path}\predictions.txt"
+    with open(save_path, 'w', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        
+        # Write data
+        for patient_id ,real, predicted_class, conf in result_from_images:
+            writer.writerow([patient_id ,real, predicted_class, conf])
+
+def predict_by_ID(model, test_folder, conf_threshold = 0.7):
 
     classes_name = os.listdir(test_folder)
 
@@ -92,31 +137,64 @@ def predict_by_ID(model, test_folder):
                 progress += 1
                 print(f"Processing {progress}/{patient_number}: {patient_image_ID}")
 
-                predicted = predict_image_folder(model, patient_image_folder)
+                predicted = predict_image_folder(model, patient_image_folder, conf_threshold)
                 results.append((class_name, predicted))
                 
     metrics = calculate_metrics(results)
     return metrics
 
 if __name__ == "__main__":
-    test_folder = r"C:\Users\acer\Desktop\Project_TMJOA\Data\test_grouped-20250924T193516Z-1-001\test_grouped"
-    model_path = r"C:\Users\acer\Desktop\Project_TMJOA\model\OA_multiview\runs\classify\MultiView_OA3\weights\best.pt"
-    save_result_path = rf"{test_folder}\result.txt"
 
-    model = YOLO(model_path)
-    metrics = predict_by_ID(model, test_folder)
-    cm = metrics['confusion_matrix']
-    pprint.pprint(metrics)
+    erosion_model_path = r"C:\Users\kanan\Desktop\Project_TMJOA\model\erosion_multiview\content\runs\classify\MultiView_erosion\weights\best.pt"
+    erosion_data_path = r"d:\Kananat\Data\training_dataset_2D\Multiview\erosion_multiview\test_grouped"
 
-    with open(save_result_path, 'w') as file:
+    flattenning_model_path = r"C:\Users\kanan\Desktop\Project_TMJOA\model\flattening_multiview\content\runs\classify\MultiView_flattening\weights\best.pt"
+    flattenning_data_path = r"d:\Kananat\Data\training_dataset_2D\Multiview\flattening_multiview\test_grouped"
+
+    genSclerosis_model_path = r"C:\Users\kanan\Desktop\Project_TMJOA\model\genSclerosis_cont_multiview\content\runs\classify\genSclerosis_cont\weights\best.pt"
+    genSclerosis_data_path = r"d:\Kananat\Data\training_dataset_2D\Multiview\genSclerosis_multiview\test_grouped"
+
+    OA_model_path = r"C:\Users\kanan\Desktop\Project_TMJOA\model\OA_multiview\runs\classify\MultiView_OA3\weights\best.pt"
+    OA_data_path = r"d:\Kananat\Data\training_dataset_2D\Multiview\OA_multiview\test_grouped"
+
+    osteophyte_model_path = r"C:\Users\kanan\Desktop\Project_TMJOA\model\osteophyte_multiview\content\runs\classify\osteophyte\weights\best.pt"
+    osteophyte_data_path = r"d:\Kananat\Data\training_dataset_2D\Multiview\osteophyte_multiview\test_grouped"
+
+    subCyst_model_path = r"C:\Users\kanan\Desktop\Project_TMJOA\model\subCyst_multiview\content\runs\classify\subCyst\weights\best.pt"
+    subCyst_data_path = r"D:\Kananat\Data\training_dataset_2D\Multiview\subCyst_multiview\test_grouped"
+
+    erosion_path = (erosion_model_path, erosion_data_path)
+    flattenning_path = (flattenning_model_path, flattenning_data_path)
+    genSclerosis_path = (genSclerosis_model_path, genSclerosis_data_path)
+    OA_path = (OA_model_path, OA_data_path)
+    osteophyte_path = (osteophyte_model_path, osteophyte_data_path)
+    subCyst_path = (subCyst_model_path, subCyst_data_path)
+
+    all_path = [erosion_path, flattenning_path, genSclerosis_path, OA_path, osteophyte_path, subCyst_path]
+
+    for model_path, data_path in all_path:
+        print(f"Processing model: {data_path}")
+        model = YOLO(model_path)
+        get_prediction_results(model, data_path)
+
+    # test_folder = r"D:\Kananat\Data\training_dataset_2D\Multiview\subCyst_multiview\test_grouped"
+    # model_path = r"C:\Users\kanan\Desktop\Project_TMJOA\model\subCyst_multiview\content\runs\classify\subCyst\weights\best.pt"
+    # save_result_path = rf"{test_folder}\result.txt"
+
+    # model = YOLO(model_path)
+    # metrics = predict_by_ID(model, test_folder, conf_threshold=0.7)
+    # cm = metrics['confusion_matrix']
+    # pprint.pprint(metrics)
+
+    # with open(save_result_path, 'w') as file:
         
-        file.write(f"Accuracy: {metrics['accuracy']:.4f}\n")
-        file.write(f"Precision: {metrics['precision']:.4f}\n")
-        file.write(f"Recall: {metrics['recall']:.4f}\n")
-        file.write(f"F1 score: {metrics['f1_score']:.4f}\n")
-        file.write("           Predicted\n")
-        file.write("           0       1\n")
-        file.write(f"Actual 0  {cm['tn']:2d}      {cm['fp']:2d}\n")
-        file.write(f"       1  {cm['fn']:2d}      {cm['tp']:2d}\n")
+    #     file.write(f"Accuracy: {metrics['accuracy']:.4f}\n")
+    #     file.write(f"Precision: {metrics['precision']:.4f}\n")
+    #     file.write(f"Recall: {metrics['recall']:.4f}\n")
+    #     file.write(f"F1 score: {metrics['f1_score']:.4f}\n")
+    #     file.write("           Predicted\n")
+    #     file.write("           0       1\n")
+    #     file.write(f"Actual 0  {cm['tn']:2d}      {cm['fp']:2d}\n")
+    #     file.write(f"       1  {cm['fn']:2d}      {cm['tp']:2d}\n")
 
-    print(f"Results saved to: {save_result_path}")
+    # print(f"Results saved to: {save_result_path}")
